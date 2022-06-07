a6b1ca6bb6409d0439e477cc1452bca011422592
JakaV3 <106893964+JakaV3@users.noreply.github.com>
2022-06-07 21:16:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'Jaka' // Ganti Jadi Namamu
+  global.owner = ['6285785694474','6285785694474','6285785694474'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285785694474' // Owner Utama Istilahnya
+  global.botname = 'Sakura Botz' // Ganti Jadi Nama Botmu
+  global.packname = 'Sakura Botz' // Sticker Weem
+  global.author = 'Jaka' // Sticker Weem
+  global.webme = 'https://instagram.com/itsme_nitsu' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/ED7th5Ie38eBayZEbDPxwY' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UC7jyjV6kcG6qTV_VcJtJzzA' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/JakaV3' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/itsme_nitsu' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
